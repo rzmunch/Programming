f37f61425b36e25d0d89e27594cd4d3834973d3a
Ronald <ronaldfuchs@gmail.com>
2021-05-11 20:46:15 -0300

    Update grades.c

--- C/grades.c
+++ C/grades.c
@@ -2,10 +2,24 @@
 
 int main(void)
 {
-    int grade;
+    int grade, digit;
+    char letter;
 
-    printf("ingrese su nota: ");
+    printf("Enter numerical grade: ");
     scanf("%d", &grade);
-    swith
-
+    if (grade > 100 || grade < 0){
+        printf("Grade outside of range.");
+        return 0; 
+    }
+    
+    digit = grade/10;
+    switch (digit) {
+        case 10 : case 9 : letter='A'; break;
+        case 8 : letter='B'; break;
+        case 7 : letter='C'; break;
+        case 6 : letter='D'; break;
+        default : letter='F'; break;
+    }
+    printf("Letter grade: %c", letter);
+    return 0;
 }
